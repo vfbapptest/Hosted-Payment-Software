df38035caafeaee15f73c203af5e0b8cf8a4c298
vfbapptest <vfbapptest@gmail.com>
2015-09-15 12:34:51 -0400

    Initial Commit

---
+++ Hosted-Payment-Software/src/hostedPaymentSoftware/HostedClass.java
@@ -0,0 +1,10 @@
+package hostedPaymentSoftware;
+
+public class HostedClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is the Hosted Payment Software Class ");
+
+	}
+
+}
